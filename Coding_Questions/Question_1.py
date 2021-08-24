4d023d501bfe331c5893bec3985074bf22aa49fb
Abhinandan Raj <abhinandanraj199@gmail.com>
2021-08-24 16:45:02 +0530

    Create Question_1.py

---
+++ Coding_Questions/Question_1.py
@@ -0,0 +1,38 @@
+Write suitable arithmetic expressions for the displayed descriptions.
+
+This is meant as a practice exercise for you to get used to some basic syntax in IDLE.
+So there may be several correct responses for some of the descriptions.
+
+# 1.Sum of 21 and 62
+Solution: 21+62
+
+#2.Difference between 43 and 17
+Solution: 43-17
+
+#3.36 mod 5
+Solution: 36%5
+
+#4.Sum of 6 and 4 multiplied by 10
+Solution: (6+4)*10
+    
+#5.23 to the 5th power
+Solution: 23**5
+    
+#6.Any expression that will result in 19 and which includes multiplication
+Solution: 4*5-1
+    
+#7.Sum of 177 mod 4 and 223 mod 5
+Solution: (177%4)+(223%5)
+    
+#8.Difference between 3rd power of 14 and 3rd power of 13
+Solution: 14**3-13**3
+    
+#9.Any expression that will result in 0, which does not include multiplication and contains at least 3 arithmetic operators.
+Solution: (27%11)+10-15
+    
+#10.Any expression that includes all six arithmetic operators,
+#addition (+), subtraction (-), multiplication (*), division (/), modulo (%) and power (**).
+Solution: ((20*3+5-8)/3**2)%4
+    
+
+
