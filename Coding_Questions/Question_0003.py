1de743fd568f4f302ad9629d0196fb32957ac3c4
Abhinandan Raj <abhinandanraj199@gmail.com>
2021-08-28 18:34:13 +0530

    Create Question_0003.py

---
+++ Coding_Questions/Question_0003.py
@@ -0,0 +1,19 @@
+#1.Display the length of the dictionary.
+Solution: len(villians)
+Output: 6
+
+#2.Display the key which has the least value.
+Solution: min(villians)
+Output: 1
+
+#3.Display the value of the key 9 as well as delete it simultaneously.
+Solution: villians.pop(9)
+Output: 'Green Goblin'
+
+#4.Display all the values in dictionary.
+Solution: villians.values()
+Output: dict_values(['magneto', 'Joker', 'Doctor Doom', 'Lex Luthor', 'Loki'])
+
+#5.Delete all the elements in the dictionary while retaining the empty object.
+Solution: villians.clear()
+Output: villians={}
